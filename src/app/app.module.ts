801cc875aca8f79ca7e5eb54f9d5ded287ad9df3
Rodrigo Franco Junior <89923374+rodrigojr08@users.noreply.github.com>
2021-09-10 09:59:31 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './views/home/home.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import { CalculadoraComponent } from './viwes/home/calculadora/calculadora.component';
+import {MatTabsModule} from '@angular/material/tabs';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    CalculadoraComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTabsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
